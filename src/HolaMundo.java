811fa8004987efe2e128edd0c6406d572ac6843b
daniinigogarcia98 <149794707+daniinigogarcia98@users.noreply.github.com>
2023-11-24 13:07:05 +0100

    pregunta

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -3,8 +3,9 @@ public class HolaMundo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hola Bienvenido al ProyectoUlises");
-		System.out.println("Hola pepito");
+		System.out.println("Hola don jose");
+		System.out.println("Hola don pepito");
+		System.out.println("Paso usted por mi casa");
 	}
 
 }
